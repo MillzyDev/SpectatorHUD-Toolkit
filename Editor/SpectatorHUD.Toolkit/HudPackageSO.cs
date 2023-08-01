fbf54374ea6275b3751d4b292df70a859f33c490
Millzy <millzyyt08@gmail.com>
2023-08-01 13:48:43 +0100

    hud package

---
+++ Editor/SpectatorHUD.Toolkit/HudPackageSO.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace SpectatorHUD.Toolkit
+{
+    // ReSharper disable once InconsistentNaming
+    public class HudPackageSO : ScriptableObject
+    {
+        public GameObject hudObject;
+        public HudManifestSO hudManifest;
+        public HudConfigSO hudConfig;
+    }
+}
